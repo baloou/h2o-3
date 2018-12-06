7c264e50ae7ae664156d82aba34e9e35680a6b87
Erin LeDell <erin@h2o.ai>
2018-12-06 07:25:59 +0100

    Formatting of h2o-r/tests/testdir_jira/runit_pubdev_6019.R
    Co-Authored-By: Pscheidl <pavel.junior@pscheidl.cz>

--- h2o-r/tests/testdir_jira/runit_pubdev_6019.R
+++ h2o-r/tests/testdir_jira/runit_pubdev_6019.R
@@ -5,7 +5,7 @@ source("../../scripts/h2o-r-test-setup.R")
 
 test.timestamp <- function() {
   prostate.data <- h2o.importFile(path = locate('smalldata/logreg/prostate_train.csv'))
-  gbm.model <- h2o.gbm(x=c("AGE","RACE","DPROS"),y="CAPSULE",training_frame=prostate.data ,model_id="gbm_prostate_model", ntrees = 1, max_depth = 1, seed = 1)
+  gbm.model <- h2o.gbm(x = c("AGE","RACE","DPROS"), y = "CAPSULE", training_frame = prostate.data, model_id = "gbm_prostate_model", ntrees = 1, max_depth = 1, seed = 1)
   expect_false(is.null(gbm.model@model$timestamp))
 
 }
